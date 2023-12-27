790e43c9c3fb480a222df3fc2a5d56449a71c6a4
b4dmilk <bp5v@mail.ru>
2023-12-27 14:14:09 +0300

    add inlineKeyboards in handlers/other.py + webApp

--- app/handlers/other.py
+++ app/handlers/other.py
@@ -1,15 +1,22 @@
 from aiogram import Router, F
-from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from aiogram.filters.state import StateFilter, StatesGroup, State
-from keyboards.keyboards import get_main_kb
 from aiogram.fsm.context import FSMContext
 from database.db import create_article
+#from keyboards.inlineKeyboards import cmd_random, get_inline_keyboard
+from aiogram.types.web_app_info import WebAppInfo
+
+
 
 router = Router()
 
-
+ABOUT = """Hi, my dear friend! 
+        It's my contacts: 
+"""
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import Message
 
 class createNewArticle(StatesGroup):
     name = State()
@@ -59,4 +66,18 @@ async def add_name(message: Message, state: FSMContext):
     await message.answer( text=f"<b>{data['name']}</b>\n {data['desc']}\n{data['article']}", parse_mode= "HTML")
     await state.clear()
 
-
+@router.message(F.text == "About Me")
+async def about_info(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(
+        text="GitHub",  
+        web_app=WebAppInfo(url="https://github.com/b4dmilk"),)
+        )
+    builder.row(InlineKeyboardButton(
+        text="Оф. канал Telegram",
+        url="https://t.me/badmi1k")
+    )
+    await message.answer(
+    text=ABOUT, parse_mode="HTML", reply_markup=builder.as_markup(),
+     )
+  
--- app/keyboards/keyboards.py
+++ app/keyboards/keyboards.py
@@ -1,10 +1,12 @@
-from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
+from aiogram.types.web_app_info import WebAppInfo
 def get_main_kb() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text="/register")
     kb.button(text="/help")
+    kb.button(text="About Me")
+    kb.button(text="/addArticle")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True,input_field_placeholder="What you want?")
 
